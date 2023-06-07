6baf95f990225a69d551e7caa84bfbf354fda723
nadiaaiello <85378655+nadiaaiello@users.noreply.github.com>
2023-06-07 12:47:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const codigo=12345
+let codigoIngresado=prompt("Ingresa el codigo de 5 digitos (Usa los numeros dados al final de cada mision.)")
+
+while (codigoIngresado!=codigo){
+     codigoIngresado=prompt("El codigo ingresado es incorrecto. Intenta de nuevo")
+}
