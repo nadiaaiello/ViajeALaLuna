5b20455d1cb9fb06d61057057ed086c622e7368a
nadiaaiello <85378655+nadiaaiello@users.noreply.github.com>
2023-06-07 12:51:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ let codigoIngresado=prompt("Ingresa el codigo de 5 digitos (Usa los numeros dado
 while (codigoIngresado!=codigo){
      codigoIngresado=prompt("El codigo ingresado es incorrecto. Intenta de nuevo")
 }
+alert("Gracias por jugar. Bienvenido a la luna")
